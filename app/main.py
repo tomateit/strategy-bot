87fa0a34ee8e96dd3d829b929c3b1ace408f2627
Max Ermakov <maksim_ermakov@protonmail.com>
2021-05-04 13:20:04 +0000

    Strategy drafts

--- app/main.py
+++ app/main.py
@@ -9,9 +9,13 @@ import tinvest as ti
 async def main():
     async with ti.Streaming(TOKEN) as streaming:
         await streaming.candle.subscribe('BBG0013HGFT4', ti.CandleResolution.min1)
-        await streaming.orderbook.subscribe('BBG0013HGFT4', 5)
+        # await streaming.orderbook.subscribe('BBG0013HGFT4', 5)
         await streaming.instrument_info.subscribe('BBG0013HGFT4')
         async for event in streaming:
             print(event)
 
-asyncio.run(main())
+if __name__=="__main__":
+    try:
+        asyncio.run(main())
+    except KeyboardInterrupt:
+        print("Closing")
